feb48dce4b0a006cf6e139beebb324d0cc17c728
ChriskenM <chriskenmutuma0727@gmail.com>
2024-01-19 02:50:58 -0800

    1 fixing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#installs package flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
