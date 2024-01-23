768910ca5675ef9092b95984c53189a6c0e6d23c
ChriskenM <chriskenmutuma0727@gmail.com>
2024-01-23 07:34:25 -0800

    all files

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+include stdlib
+#Configuring web-server using puppet
+
+package { 'ngix':
+  ensure => installed,
+}
+
+file_line { 'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me github.com/ChriskenM permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
---
+++ 0x0C-web_server/README.md
@@ -0,0 +1,14 @@
+# **Web server**
+
++ the main role of a web server
++ What is a child process
++ Why web servers usually have a parent process and child processes
++ the main HTTP requests
+
+# **Requirements**
+
++  files will be interpreted on Ubuntu 16.04 LTS
++ All your Bash script files must be executable
++ Bash script must pass Shellcheck (version 0.3.7) without any error
++ a comment explaining what is the script doing
++ You can’t use systemctl for restarting a process
